421b79b7dc457494aeb64a288ca384ac90c76fe1
dojey <116774898+dojey@users.noreply.github.com>
2022-10-26 23:18:07 -0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
 # args
-CADDYIndexPage="https://raw.githubusercontent.com/caddyserver/dist/master/welcome/index.html
+CADDYIndexPage="https://raw.githubusercontent.com/caddyserver/dist/master/welcome/index.html"
 
 # configs
 mkdir -p /etc/caddy/ /etc/xray/ /usr/share/caddy && echo -e "User-agent: *\nDisallow: /" >/usr/share/caddy/robots.txt
 #wget $CADDYIndexPage -O /usr/share/caddy/index.html && unzip -qo /usr/share/caddy/index.html -d /usr/share/caddy/ && mv /usr/share/caddy/*/* /usr/share/caddy/
-wget $CADDYIndexPage -O /usr/share/caddy/index.html 
+wget $CADDYIndexPage -O /usr/share/caddy/index.html
 cp -f /conf/Caddyfile /etc/caddy/Caddyfile
 cp -f /conf/config.json /etc/xray/config.json
 
